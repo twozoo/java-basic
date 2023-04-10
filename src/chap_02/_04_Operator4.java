594800f7e5f9df56a8f713d91b632a6c72c81585
twozoo <joo087@naver.com>
2023-04-10 12:43:27 +0900

    연산자 커밋

---
+++ src/chap_02/_04_Operator4.java
@@ -0,0 +1,21 @@
+package chap_02;
+
+public class _04_Operator4 {
+    public static void main(String[] args) {
+        // 논리연산자
+        boolean 김치찌개 = false;
+        boolean 계란말이 = true;
+        boolean 제육볶음 =  true;
+        System.out.println( 김치찌개 || 계란말이 || 제육볶음);
+        System.out.println( 김치찌개 && 계란말이 && 제육볶음);
+
+        System.out.println(5>3 && 2<4);
+        System.out.println(5>3 && 2>4);
+
+        System.out.println(!true);
+        System.out.println(!false);
+
+
+
+    }
+}
---
+++ src/chap_02/_Quiz_02.java
@@ -0,0 +1,31 @@
+package chap_02;
+
+public class _Quiz_02 {
+
+    /*
+    * 어린이 키에 따른 놀이기구 탑승 가능 여부를 확인하는 프로그램을 작성하시오
+    *
+    * 조건
+    * 키 120 cm  이상인 경우
+    * 삼항연산자 이용
+    *
+    * 결과
+    * 키가 115cm 이므로 탑승이 불가능 합니다.
+    * 키가 121cm 이므로 탑승이 가능 합니다.
+    * */
+    public static void main(String[] args) {
+        int child1 = 115;
+        int child2 = 121;
+        int limitTall = 120;
+
+        String result = (limitTall <= child1) ? "키가 " + child1+"cm 므로 탑승이 가능 합니다." : "키가 " + child1+"cm 므로 탑승이 불가능 합니다.";
+        System.out.println(result);
+
+        result = (limitTall <= child2) ? "키가 " + child2+"cm 므로 탑승이 가능 합니다." : "키가 " + child2+"cm 므로 탑승이 불가능 합니다.";
+
+        System.out.println(result);
+
+
+
+    }
+}
