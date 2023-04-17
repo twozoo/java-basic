656af7d872934889fda9e5849a415a8888bfd32b
twozoo <joo087@naver.com>
2023-04-17 11:56:10 +0900

    클래스 메소드

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -9,4 +9,28 @@ public class BlackBox {
 
     // 클래스 변수
     static boolean canAutoBoolean = false; // 자동 신고 기능
+
+    void autoReport(){
+        if(canAutoBoolean){
+            System.out.println("충돌이 감지되어 자동으로 신고 합니다.");
+        }else{
+            System.out.println("자동 신고 기능이 지원되지 않습니다.");
+        }
+    }
+    void insertMemoryCard(int capacity){
+        System.out.println("메모리 카드가 삽입되었습니다.");
+        System.out.println("용량은 : " + capacity +"GB 입니다.");
+    }
+
+    int getVideoFileCount(int type){
+        if(type==1){ // 일반영상
+            return 9;
+
+        }else if(type==2){
+            return 1;
+        }
+        return 10;
+    }
+
+
 }
---
+++ src/chap_07/_04_Method.java
@@ -0,0 +1,21 @@
+package chap_07;
+
+public class _04_Method {
+    public static void main(String[] args) {
+        BlackBox b1 = new BlackBox();
+        b1.modelName="까망이";
+        b1.autoReport();
+        BlackBox.canAutoBoolean=true;
+        b1.autoReport();
+        b1.insertMemoryCard(256);
+
+        // 일반 영상 : 1
+
+        // 이벤트 영상(충돌 방지) : 2
+        int fileCount = b1.getVideoFileCount(1); // 일반영상
+        System.out.println("일반 영상 파일 수 : "+fileCount+"개");
+        fileCount= b1.getVideoFileCount(2); // 이벤트 영상
+        System.out.println("이벤트 영상 파일 수 : "+fileCount+"개");
+
+    }
+}
